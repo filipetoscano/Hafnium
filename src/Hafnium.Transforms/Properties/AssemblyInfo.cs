9da9df1eada4a3656116eabc8ca414225ac26476
Filipe Toscano <filipe.toscano@gmail.com>
2017-02-19 23:54:30 +0100

    * Change: Move XSD/XSLT to separate project, and create .nuget for it.

---
+++ src/Hafnium.Transforms/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General information
+[assembly: AssemblyTitle( "Hafnium Transforms" )]
+[assembly: AssemblyDescription( "Transformations and Schema for Hafnium rules." )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Transition" )]
+[assembly: AssemblyProduct( "Hafnium" )]
+[assembly: AssemblyCopyright( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Additional information
+[assembly: ComVisible( false )]
+[assembly: Guid( "a53d5f02-9b81-4a88-b458-e5f783125c0c" )]
+
+// Version information
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
